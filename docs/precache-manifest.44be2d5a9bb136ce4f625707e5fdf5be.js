10446fd8c8294bcabc5082c16e58b3920ceec58f
Sixto José Romero Martínez <sixto.jose@gmail.com>
2020-08-29 15:29:43 -0500

    Se cambia nombre carpeta de build a docs

---
+++ docs/precache-manifest.44be2d5a9bb136ce4f625707e5fdf5be.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7f645050d6daabe73abb1ca068486b5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c6137ee40edec45ca82d",
+    "url": "/static/css/main.cccf9e68.chunk.css"
+  },
+  {
+    "revision": "1094a9d0787669c6640b",
+    "url": "/static/js/2.ba604ae7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ba604ae7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c6137ee40edec45ca82d",
+    "url": "/static/js/main.8c752b5e.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
